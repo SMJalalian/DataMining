e08c7ebfcf346e4961500d5bab981b872150c5d4
Mohamad Jalalian - RugPC <SM.Jalalian@Outlook.com>
2021-01-10 13:49:33 +0330

    final ( min-max )

--- 010-MinMax Normalization.py
+++ 010-MinMax Normalization.py
@@ -7,17 +7,17 @@ scaler = MinMaxScaler()
 scaler.fit(X)
 
 X_scaled = scaler.transform(X)
-# Manually normalise without using scikit-learn
+
 X_manual_scaled = (X - X.min(axis=0)) / (X.max(axis=0) - X.min(axis=0))
-# Verify manually VS scikit-learn estimation
-print(np.allclose(X_scaled, X_manual_scaled))
-#True
-
-
+#*********************************************************************** 
 import matplotlib.pyplot as plt
-fig, axes = plt.subplots(1,3)
+fig, axes = plt.subplots(1,4)
 axes[0].scatter(X[:,0], X[:,1], c=y)
-axes[0].set_title("Original data")
+axes[0].set_title("Sepal Original")
 axes[1].scatter(X_scaled[:,0], X_scaled[:,1], c=y)
-axes[1].set_title("MinMax scaled data")
+axes[1].set_title("Sepal Scaled")
+axes[2].scatter(X[:,2], X[:,3], c=y)
+axes[2].set_title("Petal Original")
+axes[3].scatter(X_scaled[:,2], X_scaled[:,3], c=y)
+axes[3].set_title("Petal Scaled")
 plt.show()
