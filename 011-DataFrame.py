7367c642206bb57e01040ffac0bd4de81287ae9b
Mohamad Jalalian - Surface <SM.Jalalian@Outlook.com>
2021-01-12 10:06:52 +0330

    Temp

---
+++ 011-DataFrame.py
@@ -0,0 +1,53 @@
+import numpy as np
+import pandas as pd
+from Packages.MathHelper import IG_Entropy
+
+df = pd.read_csv("DetaFruit.csv")
+print(df.head())
+print(df.describe())
+conditionVaue = 499
+attrib = 'attr2'
+
+Melon = Orange = Apple = 0
+Total = len(df)
+
+for i,row in df.iterrows():
+    if row['class'] =='melon' and row[attrib] < conditionVaue :
+        Melon += 1
+    elif row['class'] =='orange' and row[attrib] < conditionVaue :
+        Orange += 1
+    elif row['class'] =='apple' and row[attrib] < conditionVaue :
+        Apple += 1
+
+All_Entropy = IG_Entropy(Melon/Total, Orange/Total, Apple/Total)
+print('Dataset Entropy: %.3f bits' % All_Entropy)
+
+#c1_total = c1_class0 + c1_class1 + c1_class2
+#c1_entropy = entropy(c1_class0/c1_total , c1_class1/c1_total , c1_class2/c1_total )
+#
+
+#dc1_class0 = 0
+#dc1_class1 = 0
+#dc1_class2 = 0
+#
+#
+#for i,row in df.iterrows():
+#    if row['class'] =='melon' and row[attrib] >= conditionVaue :
+#        dc1_class0 = dc1_class0 + 1
+#    elif row['class'] =='orange' and row[attrib] >= conditionVaue:
+#        dc1_class1 = dc1_class1 + 1
+#    elif row['class'] =='apple'  and row[attrib] >= conditionVaue:
+#        dc1_class2 = dc1_class2 + 1
+#dc1_total = dc1_class0 + dc1_class1 + dc1_class2
+#dc1_entropy = entropy(dc1_class0/dc1_total , dc1_class1/dc1_total , dc1_class2/dc1_total )
+#
+#conditionEntropy = (c1_total / total) * c1_entropy + (dc1_total/total) * dc1_entropy
+#print('Condition Entropy: %.3f bits' % conditionEntropy)
+#
+#gain = s_entropy - conditionEntropy
+#print('Information Gain: %.3f bits' % gain)
+
+
+
+
+
--- Packages/MathHelper.py
+++ Packages/MathHelper.py
@@ -1,4 +1,6 @@
 import math
+import numpy as np
+
 def Entropy(textObject):
     ent = 0
     for word in textObject.UniqueWords :
@@ -6,3 +8,15 @@ def Entropy(textObject):
         temp = (repeat/textObject.AllWordsCount) * (math.log(repeat/textObject.AllWordsCount))
         ent += temp
     return math.fabs(ent)
+#************************************************
+def IG_Entropy(class0, class1,class2):
+
+    Output1 = Output2 = Output3 = 0
+    if class0:
+           Output1 = class0 * np.log2(class0)
+    if class1:
+           Output2 = class1 * np.log2(class1)
+    if class2:
+           Output3 = class2 * np.log2(class2)
+           
+    return (-(Output1 + Output2 + Output3 ))
